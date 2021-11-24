625d67fd70219dea91649db104a56080db4aeb52
Русских Евгения <russkih.evgeniya92@yandex.ru>
2021-11-24 22:15:47 +0300

    Отрисует блоки с фильмами

--- src/main.js
+++ src/main.js
@@ -19,3 +19,39 @@ renderTemplate(siteHeaderElement, createProfileTemplate(), RenderPosition.BEFORE
 renderTemplate(siteMainElement, createMainNavigationTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(siteMainElement, createSortTemplate(), RenderPosition.BEFOREEND);
 renderTemplate(footerStatisticsElement, createFilmsCounterTemplate(), RenderPosition.BEFOREEND);
+
+renderTemplate(siteMainElement, '<section class="films"></section>', RenderPosition.BEFOREEND);
+
+const filmsElement = siteMainElement.querySelector('.films');
+
+const renderFilmsListTemplate = (title, extra) => {
+  renderTemplate(filmsElement, createFilmsListTemplate(), RenderPosition.BEFOREEND);
+
+  const listElement = filmsElement.querySelector('.films-list:last-of-type');
+  const titleElement = listElement.querySelector('.films-list__title');
+  const containerElement = listElement.querySelector('.films-list__container');
+
+  let filmsCount = FILM_COUNT;
+
+  titleElement.innerHTML = title;
+
+  switch (extra) {
+    case true:
+      listElement.classList.add('films-list--extra');
+      filmsCount = EXTRA_FILM_COUNT;
+      break;
+
+    default:
+      titleElement.classList.add('visually-hidden');
+      renderTemplate(listElement, createMoreButtonTemplate(), RenderPosition.BEFOREEND);
+      break;
+  }
+
+  for (let i = 0; i < filmsCount; i++) {
+    renderTemplate(containerElement, createFilmCardTemplate(), RenderPosition.AFTERBEGIN);
+  }  
+};
+
+renderFilmsListTemplate('All movies. Upcoming');
+renderFilmsListTemplate('Top rated', true);
+renderFilmsListTemplate('Most commented', true);
