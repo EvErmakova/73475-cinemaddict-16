af0d6bbe9f22c3c35e3318b62a55b00d0813190e
Русских Евгения <russkih.evgeniya92@yandex.ru>
2021-11-24 20:52:01 +0300

    Добавит модуль films-list и функцию для отрисовки компонентов

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import {createFilmCardTemplate} from './view/film-card-view';
+import {createFilmDetailsTemplate} from './view/film-details-view';
+import {createFilmCounterTemplate} from './view/films-counter-view';
+import {createFilmsListTemplate} from './view/films-list-view';
+import {createMainNavigationTemplate} from './view/main-navigation-view';
+import {createMoreButtonTemplate} from './view/more-button-view';
+import {createProfileViewTemplate} from './view/profile-view';
+import {createSortViewTemplate} from './view/sort-view';
+import {renderTemplate, RenderPosition} from './render.js';
+
+const FILM_COUNT = 5;
