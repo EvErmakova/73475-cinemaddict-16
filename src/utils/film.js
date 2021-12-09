e6b7e26dc268c2da6693aa5b0ab797e22318aa65
Евгения Ермакова <russkih.evgeniya92@gmail.com>
2021-12-09 09:30:54 +0300

    Исправит ошибку

--- src/utils/film.js
+++ src/utils/film.js
@@ -1,5 +1,3 @@
-import {EXTRA_FILM_COUNT} from '../const';
-
 export const getFilmsCount = (films) => ({
   watchList: films.filter((film) => film.userDetails.watchlist).length,
   alreadyWatched: films.filter((film) => film.userDetails.alreadyWatched).length,
