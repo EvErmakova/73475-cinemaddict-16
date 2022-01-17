853ce45874f6ca15c131d46ba0bc0974251a3782
Евгения Ермакова <russkih.evgeniya92@gmail.com>
2022-01-17 13:47:36 +0300

    Поправит ошибки гайдлайна

--- src/const.js
+++ src/const.js
@@ -8,7 +8,7 @@ export const EMOTIONS = ['smile', 'sleeping', 'puke', 'angry'];
 export const SortType = {
   DEFAULT: 'default',
   DATE: 'date',
-  RATING: 'rating'
+  RATING: 'rating',
 };
 
 export const ActionType = {
@@ -21,26 +21,26 @@ export const UpdateType = {
   PATCH: 'PATCH',
   MINOR: 'MINOR',
   MAJOR: 'MAJOR',
-  INIT: 'INIT'
+  INIT: 'INIT',
 };
 
 export const FilterType = {
   ALL: {type: 'all', name: 'All movies'},
   WATCHLIST: {type: 'watchlist', name: 'Watchlist'},
   HISTORY: {type: 'history', name: 'History'},
-  FAVORITES: {type: 'favorites', name: 'Favorites'}
+  FAVORITES: {type: 'favorites', name: 'Favorites'},
 };
 
 export const NoTasksTextType = {
   [FilterType.ALL.type]: 'There are no movies in our database',
   [FilterType.WATCHLIST.type]: 'There are no movies to watch now',
   [FilterType.HISTORY.type]: 'There are no watched movies now',
-  [FilterType.FAVORITES.type]: 'There are no favorite movies now'
+  [FilterType.FAVORITES.type]: 'There are no favorite movies now',
 };
 
 export const ScreenType = {
   FILMS: 'films',
-  STATS: 'stats'
+  STATS: 'stats',
 };
 
 export const StatsFilterType = {
@@ -48,10 +48,10 @@ export const StatsFilterType = {
   TODAY: {type: 'today', name: 'Today'},
   WEEK: {type: 'week', name: 'Week'},
   MONTH: {type: 'month', name: 'Month'},
-  YEAR: {type: 'year', name: 'Year'}
+  YEAR: {type: 'year', name: 'Year'},
 };
 
 export const State = {
   SAVING: 'SAVING',
-  DELETING: 'DELETING'
+  DELETING: 'DELETING',
 };
